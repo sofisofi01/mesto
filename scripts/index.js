d06d5cec142250653b18efc67d2f2534cd667916
sofisofi01 <sofi.sofi.01@mail.ru>
2022-05-11 17:04:31 +0300

    fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,20 @@
-let background = document.querySelector('.background');
-let popup = document.querySelector('.pop-up');
-let openButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.pop-up__close');
+const background = document.querySelector('.background');
+const popup = document.querySelector('.pop-up');
+const openButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.pop-up__close');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 let popupName = document.querySelector('.pop-up__name');
 let popupDescription = document.querySelector('.pop-up__description');
-let popupSave = document.querySelector('.pop-up__save');
+const popupSave = document.querySelector('.pop-up__save');
 
 function open() {
-  popup.classList.add('pop-up_opened');
   background.classList.add('background_opened');
-  popupName.setAttribute('value', `${profileName.textContent}`);
-  popupDescription.setAttribute('value', `${profileDescription.textContent}`);
+  popupName.value = profileName.textContent;
+  popupDescription.value = profileDescription.textContent;
 }
 
 function close() {
-  popup.classList.remove('pop-up_opened');
   background.classList.remove('background_opened');
 }
 
